57305715208aff20113122ef745475972635ecd2
JP852 <142391590+JP852@users.noreply.github.com>
2024-04-30 09:13:57 +0100

    Update Output.sql

--- Output.sql
+++ Output.sql
@@ -1,3 +1,13 @@
+-- We want to stack the tables on top of one another, since they have the same fields in each sheet. We can do this one of 2 ways:
+    -- Drag each table into the canvas and use a union step to stack them on top of one another
+    -- Use a wildcard union in the input step of one of the tables
+-- Some of the fields aren't matching up as we'd expect, due to differences in spelling. Merge these fields together
+-- Make a Joining Date field based on the Joining Day, Table Names and the year 2023
+-- Now we want to reshape our data so we have a field for each demographic, for each new customer (help)
+-- Make sure all the data types are correct for each field
+-- Remove duplicates (help)
+    -- If a customer appears multiple times take their earliest joining date
+
 WITH CTE AS (
 SELECT *, 'pd2023_wk04_january' as tablename FROM pd2023_wk04_january
 
